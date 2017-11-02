7fb329ae6089d570a0079509ddfb1b06bf31b2eb
Andrii Vasyliev <sol@hiqdev.com>
2017-11-02 10:17:04 +0000

    csfixed

--- src/config/params.php
+++ src/config/params.php
@@ -1,3 +1,11 @@
 <?php
+/**
+ * Data exporting to different formats for Yii2
+ *
+ * @link      https://github.com/hiqdev/yii2-export
+ * @package   yii2-export
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017, HiQDev (http://hiqdev.com/)
+ */
 
 return [];
