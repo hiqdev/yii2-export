204afd655c0adbbe41d5de41aa23df0b267e05f0
Yurii Myronchuk <bladeroot@hiqdev.com>
2021-03-17 14:53:44 +0000

    public => protected

--- src/exporters/CsvExporter.php
+++ src/exporters/CsvExporter.php
@@ -6,7 +6,7 @@ class CsvExporter extends AbstractExporter implements ExporterInterface
 {
     protected string $exportType = Type::CSV;
 
-    public function applySettings($writer)
+    protected function applySettings($writer)
     {
         return $writer
             ->setFieldDelimiter($this->settings['fieldDelimiter'])
