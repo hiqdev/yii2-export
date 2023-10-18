26ecbad86404244ceba57fa336967d681262104a
Andrii Klochok <andreyklochok@gmail.com>
2023-10-18 21:23:07 +0300

    HP-1622: progress bar refactoring

--- src/actions/BackgroundProgressAction.php
+++ src/actions/BackgroundProgressAction.php
@@ -4,43 +4,24 @@ declare(strict_types=1);
 
 namespace hiqdev\yii2\export\actions;
 
-use Exception;
-use hipanel\actions\IndexAction;
+use hipanel\actions\ProgressAction;
 use hiqdev\yii2\export\models\BackgroundExport;
 use Yii;
 
-class BackgroundProgressAction extends IndexAction
+class BackgroundProgressAction extends ProgressAction
 {
-    public function run()
+    public function init(): void
     {
-        $id = $this->controller->request->get('id');
-        header('Content-Type: text/event-stream');
-        header('Cache-Control: no-cache');
-        header('Connection: keep-alive');
-        header('X-Accel-Buffering: no');
-        while (true) {
-            try {
-                /* @var $job BackgroundExport */
-                $job = Yii::$app->exporter->getJob($id);
-                $status = $job->getStatus();
-            } catch (Exception $exception) {
-                Yii::error('Export: ' . $exception->getMessage());
-                break;
-            }
-            $json = json_encode(
+        $this->onProgress = function () {
+            $id = $this->controller->request->get('id');
+            /* @var $job BackgroundExport */
+            $job = Yii::$app->exporter->getJob($id);
+            $status = $job->getStatus();
+
+            return json_encode(
                 ['id' => $id, 'status' => $status, 'progress' => $job->getProgress()],
                 JSON_THROW_ON_ERROR
             );
-            echo "id: " . $id . PHP_EOL;
-            echo "data: " . $json . PHP_EOL;
-            echo PHP_EOL;
-            @ob_flush();
-            flush();
-            sleep(1);
-            if ($status === BackgroundExport::STATUS_SUCCESS) {
-                break;
-            }
-        }
-        die();
+        };
     }
 }
